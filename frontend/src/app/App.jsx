02004cac0f3e9f1cbf1c35f337895281c0d7b8ee
jatin <jatinvats653@gmail.com>
2026-04-18 21:47:52 +0530

    feat: add react-router for routing and update App component

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
+import './App.css'
+import {RouterProvider} from 'react-router'
+import {routes} from './app.routes'
 function App() {
 
   return (
        <>
-    <h1>hello world</h1>
+    <RouterProvider router={routes}/>
     </>
   )
 }
